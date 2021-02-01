2125abf4b8922673387b7d332442f995cb4d486c
Adriano Barbosa <adrianobarbosa_4@hotmail.com>
2021-02-01 10:32:09 -0300

    atualização final1

--- contacttura/e2e/src/app.e2e-spec.ts
+++ contacttura/e2e/src/app.e2e-spec.ts
@@ -10,6 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
+    expect(page.getTitleText()).toEqual('contacttura app is running!');
   });
 
   afterEach(async () => {
